ed7723f772501e2846ab0df463ca666eb6eec46c
Awais Tahir <theowais456@gmail.com>
2025-07-16 02:19:52 +0500

    Added google in login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-
 import './index.css'
 import HomePage from './components/HomePage.jsx'
 import LoginPage from './components/LoginPage.jsx'
